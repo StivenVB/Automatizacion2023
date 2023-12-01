3e81954edfde25747e1d0346ed91c504b49a39a7
stiven.velez <svelez@consensussa.com>
2023-11-30 22:06:24 -0500

    Imprimir datos de my car

--- ejemplo.js
+++ ejemplo.js
@@ -3,3 +3,12 @@ var myCar = {
     Modelo: "2023",
     Color: "Gris"
 }
+
+function imprimirDatosDelCarro(carro) {
+    console.log("Marca: " + carro.Marca);
+    console.log("Modelo: " + carro.Modelo);
+    console.log("Color: " + carro.Color);
+}
+
+// Llamamos a la función con los datos de tu automóvil
+imprimirDatosDelCarro(myCar);
